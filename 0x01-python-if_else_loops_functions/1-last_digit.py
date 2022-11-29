8295920196008ed23c334bc490d958982459ef24
Oluwatadel <oluwatobiadelesi>
2022-11-29 16:18:39 +0000

    rich

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -11,4 +11,4 @@ if remainder > 5:
 elif remainder == 0:
     print("{}{} is {} and is 0".format(ld, number, remainder))
 else:
-        print(f'{ld} {number} is {remainder} and is less than 6 and not 0')
+        print(f'{ld}{number} is {remainder} and is less than 6 and not 0')
