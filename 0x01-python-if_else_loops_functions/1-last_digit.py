a3b2a8cdf8e912256a460c644bea300d54d1e0dd
Oluwatadel <oluwatobiadelesi>
2022-11-29 16:11:22 +0000

    rich

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import random
+ld = ("last digit of ")
+number = random.randint(-10000, 10000)
+if number > 0:
+    remainder = number % 10
+if number < 0:
+    remainder = number % -10
+if remainder > 5:
+    print("{}{} is {} and is greater than 5".format(ld, number, remainder))
+elif remainder == 0:
+    print("{}{} is {} and is 0".format(ld, number, remainder))
+else:
+        print("{}{} is {} and is less than 6 and not 0".format(ld, number, remainder))
