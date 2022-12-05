3cfe54c576c7d78c897eb1a2c3d60bf440d09c93
Oluwatadel <oluwatobiadelesi>
2022-12-05 23:53:52 +0000

    done

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if idx < 0 or idx > (len(my_list) - 1):
-        return (my_list)
-    l = [i for i in my_list]
-    l[idx] = element
+    l = my_list.copy()
+    if idx < 0 or idx > len(my_list) - 1:
+        return my_list.copy()
+    else:
+        l[idx] = element
     return (l)
