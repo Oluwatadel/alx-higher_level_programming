1280432e5e5801adc698ed76178e77a12000cd6e
Oluwatadel <oluwatobiadelesi>
2022-12-05 23:45:35 +0000

    done

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,6 +2,6 @@
 def new_in_list(my_list, idx, element):
     if idx < 0 or idx > (len(my_list) - 1):
         return (my_list)
-    l = my_list[:]
+    l = [i for i in my_list]
     l[idx] = element
     return l
