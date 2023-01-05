7526072e7b3f3cd21347122ef52dd50d23f02a16
Oluwatadel <oluwatobiadelesi@gmail.com>
2023-01-05 15:22:34 +0000

    100

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+# 100-matrix_mul.py
+
 """Defines a matrix multiplication function."""
 
 
---
+++ 0x07-python-test_driven_development/Python.h
@@ -0,0 +1,10 @@
+#ifndef PYTHON_H
+#define PYTHON_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void print_python_string(PyObject *p);
+
+#endif /* PYTHON_H */
