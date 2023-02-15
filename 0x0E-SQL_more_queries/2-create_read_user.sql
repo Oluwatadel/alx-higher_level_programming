c0c2cdb91cb7502edd24626c1a7e17364d7f00fb
Oluwatobi Adelesi <oluwatobiadelesi@gmail.com>
2023-02-15 20:28:30 +0000

    task 2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,5 @@
-- creates database hbtn_0d_2 and user user_0d_2
--- user_0d_2 should have only SELECT privileges, password set to user_0d_2_pwd
-
-CREATE DATABASE IF NOT EXIST hbtn_0d_2;
-CREATE USER IF NOT EXIST user_0d_2;
+-- creates database and user
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+ALTER USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
