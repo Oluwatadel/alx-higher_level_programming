50dbffc1f163d08abb460e4bdee6518596521647
Oluwatobi Adelesi <oluwatobiadelesi@gmail.com>
2023-02-15 20:33:36 +0000

    all

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,4 +1,5 @@
 -- creates table force_name
 -- table description; id INT, name VARCHAR(256)
 
-CREATE TABLE IF NOT EXIST force_name (id INT, name VARCHAR(256) NOT NULL)
+
+CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
